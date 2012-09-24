8a201ae4fb1fd458a8bd862aad3dee0348575624
flindt <pfl@eal.dk>
2012-09-24 11:23:19 +0200

    added a wget download for the files

--- TT_stellaris.sh
+++ TT_stellaris.sh
@@ -22,17 +22,34 @@ mkdir -p $TARGETDIR
 
 
 # 64 bit ARM Toolchain integration + git + svn
-if [ ! -f  arm-2011.09-69-arm-none-eabi-i686-pc-linux-gnu.tar.bz2 ]
+if [ ! -f  download/arm-2011.09-69-arm-none-eabi-i686-pc-linux-gnu.tar.bz2 ]
+then
+	echo "Downloading arm-2011.09-69-arm-none-eabi-i686-pc-linux-gnu.tar.bz2 from local server"
+	 wget http://10.165.0.48:8000/arm-2011.09-69-arm-none-eabi-i686-pc-linux-gnu.tar.bz2 -Pdownload
+fi
+if [ ! -f  download/arm-2011.09-69-arm-none-eabi-i686-pc-linux-gnu.tar.bz2 ]
 then
 	echo "Please download : arm-2011.09-69-arm-none-eabi-i686-pc-linux-gnu.tar.bz2"
 	exit 1
 fi
-if [ ! -f  SW-LM3S-8264.exe ]
+
+if [ ! -f  download/SW-LM3S-8264.exe ]
+then
+	echo "Downloading SW-LM3S-8264.exe from local server"
+	 wget http://10.165.0.48:8000/SW-LM3S-8264.exe -Pdownload
+fi
+if [ ! -f  download/SW-LM3S-8264.exe ]
 then
 	echo "Please download : SW-LM3S-8264.exe"
 	exit 1
 fi
-if [ ! -f eclipse_juno_svn_arm_gdb_64.tar.gz  ]
+
+if [ ! -f  download/eclipse_juno_svn_arm_gdb_64.tar.gz ]
+then
+	echo "Downloading eclipse_juno_svn_arm_gdb_64.tar.gz from local server"
+	 wget http://10.165.0.48:8000/eclipse_juno_svn_arm_gdb_64.tar.gz -Pdownload
+fi
+if [ ! -f download/eclipse_juno_svn_arm_gdb_64.tar.gz  ]
 then
 	echo "Please download : eclipse_juno_svn_arm_gdb_64.tar.gz"
 	exit 1
@@ -40,11 +57,11 @@ fi
 
 
 # Extract eclipse
-tar xzf eclipse_juno_svn_arm_gdb_64.tar.gz -C $TARGETDIR
+tar xzf download/eclipse_juno_svn_arm_gdb_64.tar.gz -C $TARGETDIR
 
-tar xf arm-2011.09-69-arm-none-eabi-i686-pc-linux-gnu.tar.bz2 -C $TARGETDIR
+tar xf download/arm-2011.09-69-arm-none-eabi-i686-pc-linux-gnu.tar.bz2 -C $TARGETDIR
 
-unzip SW-LM3S-8264.exe -d $TARGETDIR/stellarisware
+unzip download/SW-LM3S-8264.exe -d $TARGETDIR/stellarisware
 
 
 
